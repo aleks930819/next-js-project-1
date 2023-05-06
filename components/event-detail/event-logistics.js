9bdb4df553fdae6847c819a560ce45ce9e8faefd
Aleksandar Atanasov <aleks930819@gmail.com>
2023-05-06 13:18:47 +0300

    added event detail

---
+++ components/event-detail/event-logistics.js
@@ -0,0 +1,30 @@
+import classes from './event-logistics.module.css';
+
+function EventLogistics(props) {
+  const { date, address, image, imageAlt } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const addressText = address.replace(', ', '\n');
+
+  return (
+    <section className={classes.logistics}>
+      <div className={classes.image}>
+        <img src={`/${image}`} alt={imageAlt} />
+      </div>
+      <ul className={classes.list}>
+        <li>
+          <time>{humanReadableDate}</time>
+        </li>
+        <li>
+          <address>{addressText}</address>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default EventLogistics;
